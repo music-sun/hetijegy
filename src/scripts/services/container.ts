31f72a38d3740ce0ae1f78ccdca0b79fe9a91d49
codernr <zoltan.csore@protonmail.com>
2022-11-15 11:16:10 +0100

    #1 Fix dynamic import

--- src/scripts/services/container.ts
+++ src/scripts/services/container.ts
@@ -6,8 +6,8 @@ declare let grecaptcha: Recaptcha;
 
 const suffix = import.meta.env.DEV ? 'dev' : 'default';
 
-const serviceClientConstructor = (await import(`./service-client.${suffix}`)).default;
-const captchaServiceConstructor =  (await import(`./captcha-service.${suffix}`)).default;
+const serviceClientConstructor = (await import(`./service-client.${suffix}.ts`)).default;
+const captchaServiceConstructor =  (await import(`./captcha-service.${suffix}.ts`)).default;
 
 export const serviceClient = new serviceClientConstructor(import.meta.env.VITE_BASE_URL) as ServiceClient;
 export const captchaService = new captchaServiceConstructor(grecaptcha, import.meta.env.VITE_CAPTCHA_SITE_KEY) as CaptchaService;
